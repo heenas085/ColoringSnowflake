e394db5138b9a2880d5dbd138f86057ae7ebb0c7
heenas085 <heenas085@gmail.com>
2022-12-05 02:48:45 +0100

    edited setup file

---
+++ snowflake/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='snowflake',
+packages=['snowflake'],
+description='nowflake package to color the flakes',
+      version="1.0",
+      url='https://github.com/heenas085/ColoringSnowflake',
+      author='Heena Shaikh',
+      author_email="heenas085@gmail.com",
+install_requires= ['random', 'numpy', 'turtle'],
+      )
