1301d893ca8a36f51d54f4875a89a6aa4bf14216
heenas085 <heenas085@gmail.com>
2022-12-05 04:38:12 +0100

    done

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
-setup(
-    name='foo',
-    version='1.0',
-    packages=find_packagees(),
-
-)
+setup(name='coloringsnowflake',
+packages=['coloringsnowflake'],
+description='coloringsnowflake package to color the flakes',
+      version="1.0",
+      url='https://github.com/heenas085/ColoringSnowflake',
+      author='Heena Shaikh',
+      author_email="heenas085@gmail.com",
+install_requires= ['random', 'numpy', 'turtle'],
+      )
