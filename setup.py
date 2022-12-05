77bf61daee1c88e391330e60c971081fc3ebf2a0
heenas085 <heenas085@gmail.com>
2022-12-05 02:08:13 +0100

    init file is added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='coloringsnowflake',
+      version="1.0",
+      description='coloringsnowflake package to color the flakes',
+      author='Heena Shaikh',
+      author_email="heenas085@gmail.com",
+      url='https://github.com/heenas085/ColoringSnowflake',
+      license='unlicense',
+
+      packages=[coloringsnowflake],
+      zip_safe=False,
+install_requires= ['random', 'numpy', 'turtle'],
+      )
