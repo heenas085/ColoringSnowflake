44e8a2afd1fc5002fddfafb6a0db0dde9b136892
heenas085 <heenas085@gmail.com>
2022-12-05 02:45:54 +0100

    edited setup file

---
+++ snowflake/__init__.py
@@ -0,0 +1,3 @@
+from let_it_snow import main
+
+main()
---
+++ snowflake/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='snowflake',
+    version='1.0',
+    scripts=['let_it_snow.py'],
+install_requires= ['random', 'numpy', 'turtle'],
+
+)
