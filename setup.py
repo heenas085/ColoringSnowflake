b3181afbe8b8c98190ee97495fb76b2659e462de
heenas085 <heenas085@gmail.com>
2022-12-05 05:29:12 +0100

    domne

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-import setuptools
-setuptools.setup(
-     name='coloringsnowflake',
-     version='0.1',
-     scripts=['let_it_snow'] ,
-     url="https://github.com/heenas085/ColoringSnowflake",
-description='snowflake package to color the flakes',
+from setuptools import setup, find_packages
+
+setup(name='snowflake',
+      packages=['snowflake'],
+      description='snowflake package to color the flakes',
+      version="1.0",
+      url='https://github.com/heenas085/ColoringSnowflake',
       author='Heena Shaikh',
       author_email="heenas085@gmail.com",
-install_requires= ['random', 'numpy', 'turtle'],
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+      install_requires=['random', 'numpy', 'turtle'],
+      )
